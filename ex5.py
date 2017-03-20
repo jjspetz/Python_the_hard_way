189f4ff9a4b768891d4addd0e01e79e163722272
JJ <jj.spetseris@yahoo.com>
2017-03-20 09:01:33 -0500

    initial commit

---
+++ ex5.py
@@ -0,0 +1,16 @@
+name = 'JJ Spetseris'
+my_age = 34
+my_height = 67 #inches
+my_weight = 150 #lbs
+my_eyes = 'brown'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
