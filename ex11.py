eadf1fe6ff5b2a6299cba66d0e1a154835373511
JJ <jj.spetseris@yahoo.com>
2017-04-16 10:03:45 -0500

    initial commit

---
+++ ex11.py
@@ -0,0 +1,15 @@
+'''
+Lesson 11: Asking Questions
+'''
+
+print "How old are you?",
+age = raw_input()
+
+print "How tall are you?",
+height = raw_input()
+
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So you are %r years old, %r inches tall and %r pounds heavy." % (
+    age, height, weight)
